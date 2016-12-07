a5c7b7098d467f573b67c9186806aff76ae15e76
Dominik Widomski <dominik@digital-detox.co.uk>
2016-12-07 16:27:00 +0000

    User roles (#2)
    * Populate user role on session level
    
    * Restrict rendering of navigation items based on role
    
    * Remove restricted items from navigation
    
    * Save draft when editing item as contributor
    
    * Update CRUD operations with regards to contributor
    
    * Add alert for mass delete error
    
    * Update role navigation restrictions
    
    * Fix linting issues
    
    * Technical review

--- admin/server/routes/index.js
+++ admin/server/routes/index.js
@@ -4,6 +4,58 @@ var path = require('path');
 
 var templatePath = path.resolve(__dirname, '../templates/index.html');
 
+const acl = {
+	contributor: {
+		users: [
+			'User',
+			'UserRole',
+		],
+		shared: [
+			'PostCategory',
+		],
+		homepage: [
+			'Benefit',
+			'DesOverview',
+		],
+		about: [
+			'AboutSection',
+			'AboutPrinciple',
+			'AboutFeature',
+			'AboutProcessItem',
+			'Skill',
+		],
+	},
+};
+
+/**
+ * Evaluate if section is accessible
+ * Section is accessible by default if there is no ACL entry for user.role.key
+ *
+ * @param {string} key
+ *
+ * @return {boolean}
+ */
+function isSectionAccessible (key, access) {
+	return !access || !access.includes(key);
+}
+
+/**
+ * Filter out sections that should not be visible to a given user
+ *
+ * @param {Object[]} sections
+ * @param {string[]} access
+ *
+ * @return {*}
+ */
+function restrictNav (sections, access) {
+	if (Array.isArray(sections) && access) {
+		return sections
+			.filter(section => isSectionAccessible(section.key, access));
+	}
+
+	return sections;
+}
+
 module.exports = function IndexRoute (req, res) {
 	var keystone = req.keystone;
 	var lists = {};
@@ -12,7 +64,6 @@ module.exports = function IndexRoute (req, res) {
 	});
 
 	var UserList = keystone.list(keystone.get('user model'));
-	const UserRole = keystone.list('UserRole');
 
 	var orphanedLists = keystone.getOrphanedLists().map(function (list) {
 		return _.pick(list, ['key', 'label', 'path']);
@@ -25,93 +76,102 @@ module.exports = function IndexRoute (req, res) {
 		backUrl = '/';
 	}
 
-	/**
-	 * Retrieve the UserRole by ID
-	 *
-	 * @param {User} user
-	 *
-	 * @return {Promise}
-	 */
-	function getUserRole (user) {
-		if (!user || !user.role) {
-			return Promise.resolve();
-		}
+	/* Restricting NAV */
+	const user = req.user;
 
-		return UserRole.model.findById(Array.isArray(user.role) ? user.role[0] : user.role);
+	keystone.roleNav = keystone.roleNav || {
+		default: _.cloneDeep(keystone.nav),
+	};
+
+	if (user && user.role && !keystone.roleNav[user.role.key]) {
+		keystone.roleNav[user.role.key] = _.cloneDeep(keystone.roleNav.default);
 	}
 
-	getUserRole(req.user)
-		.then(userRole => {
-			var keystoneData = {
-				adminPath: '/' + keystone.get('admin path'),
-				appversion: keystone.get('appversion'),
-				backUrl: backUrl,
-				brand: keystone.get('brand'),
-				csrf: { header: {} },
-				devMode: !!process.env.KEYSTONE_DEV,
-				lists: lists,
-				nav: keystone.nav,
-				orphanedLists: orphanedLists,
-				signoutUrl: keystone.get('signout url'),
-				user: {
-					id: req.user.id,
-					name: UserList.getDocumentName(req.user) || '(no name)',
-					role: userRole,
-				},
-				userList: UserList.key,
-				version: keystone.version,
-				wysiwyg: { options: {
-					enableImages: keystone.get('wysiwyg images') ? true : false,
-					enableCloudinaryUploads: keystone.get('wysiwyg cloudinary images') ? true : false,
-					enableS3Uploads: keystone.get('wysiwyg s3 images') ? true : false,
-					additionalButtons: keystone.get('wysiwyg additional buttons') || '',
-					additionalPlugins: keystone.get('wysiwyg additional plugins') || '',
-					additionalOptions: keystone.get('wysiwyg additional options') || {},
-					overrideToolbar: keystone.get('wysiwyg override toolbar'),
-					skin: keystone.get('wysiwyg skin') || 'keystone',
-					menubar: keystone.get('wysiwyg menubar'),
-					importcss: keystone.get('wysiwyg importcss') || '',
-				} },
-			};
-			keystoneData.csrf.header[keystone.security.csrf.CSRF_HEADER_KEY] = keystone.security.csrf.getToken(req, res);
+	if (user && user.role && acl[user.role.key]) {
+		console.log(`Restricting nav for ${user.role.key}`);
 
-			var codemirrorPath = keystone.get('codemirror url path')
-				? '/' + keystone.get('codemirror url path')
-				: '/' + keystone.get('admin path') + '/js/lib/codemirror';
+		const access = acl[user.role.key];
 
-			var locals = {
-				adminPath: keystoneData.adminPath,
-				cloudinaryScript: false,
-				codemirrorPath: codemirrorPath,
-				env: keystone.get('env'),
-				fieldTypes: keystone.fieldTypes,
-				ga: {
-					property: keystone.get('ga property'),
-					domain: keystone.get('ga domain'),
-				},
-				keystone: keystoneData,
-				title: keystone.get('name') || 'Keystone',
-			};
-
-			var cloudinaryConfig = keystone.get('cloudinary config');
-			if (cloudinaryConfig) {
-				var cloudinary = require('cloudinary');
-				var cloudinaryUpload = cloudinary.uploader.direct_upload();
-				keystoneData.cloudinary = {
-					cloud_name: keystone.get('cloudinary config').cloud_name,
-					api_key: keystone.get('cloudinary config').api_key,
-					timestamp: cloudinaryUpload.hidden_fields.timestamp,
-					signature: cloudinaryUpload.hidden_fields.signature,
-				};
-				locals.cloudinaryScript = cloudinary.cloudinary_js_config();
-			};
-
-			ejs.renderFile(templatePath, locals, {}, function (err, str) {
-				if (err) {
-					console.error('Could not render Admin UI Index Template:', err);
-					return res.status(500).send(keystone.wrapHTMLError('Error Rendering Admin UI', err.message));
+		keystone.roleNav[user.role.key].sections
+			.forEach(function (section, index, sections) {
+				section.lists = restrictNav(section.lists, access[section.key]);
+				if (section.lists.length === 0) {
+					sections.splice(sections.indexOf(section), 1);
 				}
-				res.send(str);
 			});
-		});
+	}
+	keystone.nav = keystone.roleNav[user.role.key];
+	/* Restricting NAV */
+
+	var keystoneData = {
+		adminPath: '/' + keystone.get('admin path'),
+		appversion: keystone.get('appversion'),
+		backUrl: backUrl,
+		brand: keystone.get('brand'),
+		csrf: { header: {} },
+		devMode: !!process.env.KEYSTONE_DEV,
+		lists: lists,
+		nav: keystone.nav,
+		orphanedLists: orphanedLists,
+		signoutUrl: keystone.get('signout url'),
+		user: {
+			id: req.user.id,
+			name: UserList.getDocumentName(req.user) || '(no name)',
+			role: req.user.role,
+		},
+		userList: UserList.key,
+		version: keystone.version,
+		wysiwyg: { options: {
+			enableImages: keystone.get('wysiwyg images') ? true : false,
+			enableCloudinaryUploads: keystone.get('wysiwyg cloudinary images') ? true : false,
+			enableS3Uploads: keystone.get('wysiwyg s3 images') ? true : false,
+			additionalButtons: keystone.get('wysiwyg additional buttons') || '',
+			additionalPlugins: keystone.get('wysiwyg additional plugins') || '',
+			additionalOptions: keystone.get('wysiwyg additional options') || {},
+			overrideToolbar: keystone.get('wysiwyg override toolbar'),
+			skin: keystone.get('wysiwyg skin') || 'keystone',
+			menubar: keystone.get('wysiwyg menubar'),
+			importcss: keystone.get('wysiwyg importcss') || '',
+		} },
+	};
+	keystoneData.csrf.header[keystone.security.csrf.CSRF_HEADER_KEY] = keystone.security.csrf.getToken(req, res);
+
+	var codemirrorPath = keystone.get('codemirror url path')
+		? '/' + keystone.get('codemirror url path')
+		: '/' + keystone.get('admin path') + '/js/lib/codemirror';
+
+	var locals = {
+		adminPath: keystoneData.adminPath,
+		cloudinaryScript: false,
+		codemirrorPath: codemirrorPath,
+		env: keystone.get('env'),
+		fieldTypes: keystone.fieldTypes,
+		ga: {
+			property: keystone.get('ga property'),
+			domain: keystone.get('ga domain'),
+		},
+		keystone: keystoneData,
+		title: keystone.get('name') || 'Keystone',
+	};
+
+	var cloudinaryConfig = keystone.get('cloudinary config');
+	if (cloudinaryConfig) {
+		var cloudinary = require('cloudinary');
+		var cloudinaryUpload = cloudinary.uploader.direct_upload();
+		keystoneData.cloudinary = {
+			cloud_name: keystone.get('cloudinary config').cloud_name,
+			api_key: keystone.get('cloudinary config').api_key,
+			timestamp: cloudinaryUpload.hidden_fields.timestamp,
+			signature: cloudinaryUpload.hidden_fields.signature,
+		};
+		locals.cloudinaryScript = cloudinary.cloudinary_js_config();
+	};
+
+	ejs.renderFile(templatePath, locals, {}, function (err, str) {
+		if (err) {
+			console.error('Could not render Admin UI Index Template:', err);
+			return res.status(500).send(keystone.wrapHTMLError('Error Rendering Admin UI', err.message));
+		}
+		res.send(str);
+	});
 };
